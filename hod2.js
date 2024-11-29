977ebdf222ae7fef24ebdb8ebaed4bd19212e3db
Ayon07-ah <ayonhazra1001ah@gmail.com>
2024-11-29 12:24:13 +0530

    HOD 2

---
+++ hod2.js
@@ -0,0 +1,7 @@
+function removeStudent() {
+    document.getElementsByClassName("confirmation")[0].style.display = "flex";
+}
+
+function closeConfirmation() {
+    document.getElementsByClassName("confirmation")[0].style.display = "none";
+}
