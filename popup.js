cde56f409c9fd3ffe2ac5068c11ea4ceaf71d51c
Ayon07-ah <ayonhazra1001ah@gmail.com>
2024-12-04 17:41:23 +0530

    exam form and enroll popup

---
+++ popup.js
@@ -0,0 +1,15 @@
+function openConfirmation1() {
+    document.getElementsByClassName("enrollment")[0].style.display = "flex";
+}
+
+function closeConfirmation1() {
+    document.getElementsByClassName("enrollment")[0].style.display = "none";
+}
+
+function openConfirmation2() {
+    document.getElementsByClassName("examform")[0].style.display = "flex";
+}
+
+function closeConfirmation2() {
+    document.getElementsByClassName("examform")[0].style.display = "none";
+}
