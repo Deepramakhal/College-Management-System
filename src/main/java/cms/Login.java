9e7ef3273de8c1825f0306532a1898d1abe59cf4
Debesh Mandal <debesh2304@gmail.com>
2024-12-06 20:18:02 +0530

    update login.java with decryp

--- src/main/java/cms/Login.java
+++ src/main/java/cms/Login.java
@@ -1,17 +1,23 @@
 package cms;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import models.Admin;
+import models.Faculty;
+import models.Student;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+
+@WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -27,28 +33,29 @@ public class Login extends HttpServlet {
 		{
 			if(role.equals("HOD")||role.equals("PRINCIPAL"))
 			{
-				PreparedStatement ps=con.prepareStatement("select password from admin where email=? and id=?");
+				PreparedStatement ps=con.prepareStatement("select * from admin where email=? and id=?");
                 ps.setString(1, email);
 				ps.setString(2, id);
 				ResultSet rs=ps.executeQuery();
 				if(rs.next())
 				{
-					if(rs.getString(1).equals(password))
+					String pwd = rs.getString("password"); 
+					 if (BCrypt.checkpw(password, pwd))
 					{
+						Admin admin=new Admin(rs.getString("id"),rs.getString("name"),rs.getString("email"),rs.getString("phone"),rs.getString("adminType"),rs.getString("password"),rs.getString("profileImage"),rs.getString("address"));
 						HttpSession s=request.getSession();
-						s.setAttribute("uid", id);
-						s.setAttribute("role", role);
-						response.sendRedirect("index.jsp");
+						s.setAttribute("admin", admin);
+						response.sendRedirect("adminDash.jsp");
 					}
 					else
 					{
-						response.sendRedirect("login.jsp");
+						response.sendRedirect("login.jsp?msg=invalid password");
 					}
 				}
 				
 				else
 				{
-					response.sendRedirect("login.jsp");
+					response.sendRedirect("login.jsp?msg=invalid id or email");
 				}
 			}
 			else if(role.equals("STUDENT"))
@@ -59,11 +66,12 @@ public class Login extends HttpServlet {
 				ResultSet rs=ps.executeQuery();
 				if(rs.next())
 				{
-					if(rs.getString(1).equals(password))
+					String pwd = rs.getString(1);
+					 if (BCrypt.checkpw(password, pwd))
 					{
+						Student student = new Student(rs.getInt("id"),rs.getString("name"),rs.getString("email"),rs.getString("phone"),rs.getString("guardian_name"),rs.getString("gender"),rs.getString("dob"),rs.getString("batch_id"),rs.getInt("current_sem"),rs.getString("password"),rs.getString("profileImage"),rs.getString("address"));
 						HttpSession s=request.getSession();
-						s.setAttribute("uid", id);
-						s.setAttribute("role", role);
+						s.setAttribute("student",student);
 						response.sendRedirect("index.jsp");
 					}
 					else
@@ -84,11 +92,12 @@ public class Login extends HttpServlet {
 				ResultSet rs=ps.executeQuery();
 				if(rs.next())
 				{
-					if(rs.getString(1).equals(password))
+					String pwd = rs.getString("password");
+					 if (BCrypt.checkpw(password, pwd))
 					{
+						Faculty faculty = new Faculty(rs.getString("id"),rs.getString("name"),rs.getString("email"),rs.getString("phone"),rs.getString("course_id"),rs.getString("password"),rs.getString("profile_image"),rs.getString("address"));
 						HttpSession s=request.getSession();
-						s.setAttribute("uid", id);
-						s.setAttribute("role", role);
+						s.setAttribute("faculty", faculty);
 						response.sendRedirect("index.jsp");
 					}
 					else
@@ -107,5 +116,4 @@ public class Login extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
-
 	}
