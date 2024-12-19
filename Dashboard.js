a1148d87d05697ed41e0061b83831cdbed5cebf0
misuvra2215 <suvradipkundu22032004@gmail.com>
2024-12-19 17:24:08 +0530

    Dashboad

---
+++ Dashboard.js
@@ -0,0 +1,240 @@
+// principal page 
+// principal page exam registration popup
+//HOD
+function openRegPopup(popupId) { 
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Reg-Popup').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closeRegPopup(popupId) {
+
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Reg-Popup').style.display = 'none';
+}
+// principal add notice 
+function openAddNoticePopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Add-Notice').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closeAddPopup(popupId) {
+ 
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Add-Notice').style.display = 'none';
+}
+// principal view result 
+function openviewResultPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Result').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closeviewResultPopup(popupId) {
+
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Result').style.display = 'none';
+}
+// principal view profile 
+// HOD view profile 
+// faculty view profile
+//student view profile              
+function openProfilePopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#profilePopup').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closeProfilePopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#profilePopup').style.display = 'none';
+    popup.style.display = 'none';
+}
+// principal current course 
+// HOD current course 
+function openCoursePopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#CoursePopup').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closeCoursePopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#CoursePopup').style.display = 'none';
+    popup.style.display = 'none';
+}
+
+//principal add hod
+function openhodPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#hod-popup').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closehodPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#hod-popup').style.display = 'none';
+}
+
+//principal add faculty
+function openfacultyPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#faculty-popup').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closefacultyPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#faculty-popup').style.display = 'none';
+}
+
+//principal add student 
+function openAddStudentPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#admission-popup').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closeAdmissionPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#admission-popup').style.display = 'none';
+}
+
+// hod confirm result 
+function openResultPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#ResultPopup').style.display = 'flex';
+    popup.style.display = 'flex';
+}
+function closeResultPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#ResultPopup').style.display = 'none';
+    popup.style.display = 'none';
+}
+
+// Faculty upload Assignment 
+function openAssignmentPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Assignment').style.display = 'flex';
+    popup.style.display = 'flex';
+}   
+function closeAssignmentPopup(popupId) {    
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Assignment').style.display = 'none';    
+}
+
+// faculty Marks upload 
+function openMarksPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Marks').style.display = 'flex';    
+    popup.style.display = 'flex';
+}
+function closeMarksPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Marks').style.display = 'none';    
+}
+
+// faculty upload resources 
+function openResourcesPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Resources').style.display = 'flex';    
+    popup.style.display = 'flex';
+}
+function closeResourcesPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Resources').style.display = 'none';
+}
+
+// faculty check assignment 
+function openCheckAssignmentPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#CheckAss').style.display = 'flex';    
+    popup.style.display = 'flex';
+}
+function closeCheckAssignmentPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#CheckAss').style.display = 'none';    
+}
+
+//student 
+//student view marks
+function openinternalmarks(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Internal').style.display = 'flex';    
+    popup.style.display = 'flex';
+}
+function closeinternalmarks(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Internal').style.display = 'none';    
+}   
+
+//student
+// final result 
+function openFinalResultPopup(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#FinalResult').style.display = 'flex';    
+    popup.style.display = 'flex';
+}   
+function closeFinalResultPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#FinalResult').style.display = 'none';    
+}
+//student
+// contact hod 
+function openContactHod(popupId) {
+    const popup = document.getElementById(popupId); 
+
+    document.querySelector('#contact-hod').style.display = 'flex';    
+    popup.style.display = 'flex';
+}   
+function closeContactHod(popupId) {    
+    const popup = document.getElementById(popupId);
+    document.querySelector('#contact-hod').style.display = 'none';    
+}
+// student enroll form
+function openEnroll(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#Enroll').style.display = 'flex';    
+    popup.style.display = 'flex';
+}   
+function closeenrollmentS(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#Enroll').style.display = 'none';    
+}
+// exam form 
+function openexamform(popupId) {
+    const popup = document.getElementById(popupId);
+
+    document.querySelector('#examform').style.display = 'flex';    
+    popup.style.display = 'flex';
+}   
+function closeexamfrom(popupId) {
+    const popup = document.getElementById(popupId);
+    document.querySelector('#examform').style.display = 'none';    
+}
+// view AssignmentPopup
+function openviewAssignment() {
+    document.querySelector('.Assignment-popup').style.display = 'flex';
+  }
+  function closeviewAssignment() {
+    document.querySelector('.Assignment-popup').style.display = 'none';
+  }
+
+  function openResourcesPopup() {
+    document.querySelector('#Resourse').style.display = 'flex';
+  }
+  function closeresourcePopup() {
+    document.querySelector('#Resourse').style.display = 'none';
+  }
