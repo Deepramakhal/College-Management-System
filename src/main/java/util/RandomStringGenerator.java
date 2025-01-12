160c8daf953003846f3d5b45747272b5d2ae563e
Deepra Makhal <deepra313131@gmail.com>
2025-01-12 17:10:31 +0530

    Delete src/main/java directory

--- src/main/java/util/RandomStringGenerator.java
+++
@@ -1,8 +0,0 @@
-package util;
-import java.util.UUID;
-public class RandomStringGenerator {
-	public static String generateRandomString(int length) {
-		String generatedString = UUID.randomUUID().toString().replace("-","");
-		return generatedString.substring(0, length);
-	}
-}
